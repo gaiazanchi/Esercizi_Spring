4465791f3fbb063d146bfafb66cc14d98821b782
Gaia Zanchi <gaia.zanchi2001@gmail.com>
2023-03-13 14:21:47 +0100

    exercises updated

--- swagger-1/src/main/java/co/develhope/swagger1/Swagger1Application.java
+++ swagger-1/src/main/java/co/develhope/swagger1/Swagger1Application.java
@@ -1,13 +1,13 @@
-package co.develhope.swagger2;
+package co.develhope.swagger1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Swagger2Application {
+public class Swagger1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Swagger2Application.class, args);
+		SpringApplication.run(Swagger1Application.class, args);
 	}
 
 }
---
+++ swagger-1/src/test/java/co/develhope/swagger1/Swagger1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.swagger1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swagger1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
