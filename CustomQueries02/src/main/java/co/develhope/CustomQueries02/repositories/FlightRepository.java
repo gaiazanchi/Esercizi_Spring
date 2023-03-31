4b4273cc23ef0e143a47717f9e35a329fc488b17
Gaia Zanchi <gaia.zanchi2001@gmail.com>
2023-03-31 16:05:00 +0200

    Esercizio funzionante

--- CustomQueries02/src/main/java/co/develhope/CustomQueries02/repositories/FlightRepository.java
+++ CustomQueries02/src/main/java/co/develhope/CustomQueries02/repositories/FlightRepository.java
@@ -13,7 +13,8 @@ import java.util.List;
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
     List<Flight> findAllByStatus(Status status);
 
-    @Query(value = "SELECT * FROM flights AS f WHERE f.status = :p1 OR f.status = :p2;", nativeQuery = true)
-    List<Flight> findAllFlightsWithCertainStatuses(@Param("p1") Status p1, @Param("p2") Status par);
+    @Query(value = "SELECT * FROM flights AS f WHERE f.status = :#{#p1.name()} OR f.status = :#{#p2.name()}", nativeQuery = true)
+    List<Flight> findAllFlightsWithCertainStatuses(@Param("p1") Status p1, @Param("p2") Status p2);
+    //oppure string p1 e in controller trasformi in stringa
 
 }
