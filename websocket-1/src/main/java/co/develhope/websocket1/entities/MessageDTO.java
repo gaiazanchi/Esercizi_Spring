5df129cc7e6f2f2772edb52fa5dbbc2ff5b6ffe4
Gaia Zanchi <gaia.zanchi2001@gmail.com>
2023-04-07 10:34:04 +0200

    web socket 1  done

---
+++ websocket-1/src/main/java/co/develhope/websocket1/entities/MessageDTO.java
@@ -0,0 +1,13 @@
+package co.develhope.websocket1.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDTO {
+    private String type;
+    private String message;
+}
