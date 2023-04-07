0a1717341e3be3cca468d503919da4efe013f96f
Gaia Zanchi <gaia.zanchi2001@gmail.com>
2023-04-07 11:15:55 +0200

    web socket 2 done

---
+++ websocket-2/src/main/java/co/develhope/websocket2/controller/NotificationController.java
@@ -0,0 +1,32 @@
+package co.develhope.websocket2.controller;
+
+import co.develhope.websocket2.entities.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @PostMapping("/broadcast-message")
+    public ResponseEntity sendNotificationToClient(@RequestBody MessageDTO message){
+        simpMessagingTemplate.convertAndSend("/topic/broadcast", message);
+        return ResponseEntity.ok().build();
+    }
+
+    @MessageMapping("/client-message")
+    public ResponseEntity handleMessageFromWebSocket(ClientMessageDTO message){
+        System.out.println("Arrived something on /app/client-message : " + message);
+        MessageDTO messageDTO = new MessageDTO(message.getClientName(),message.getClientAlert(), message.getClientMsg());
+        simpMessagingTemplate.convertAndSend("/topic/broadcast", messageDTO);
+        return ResponseEntity.ok().build();
+    }
+
+}
